db079886fd892fffc9f029f6e3f3331f46b1e446
Sofia Wicker <sofiazw@gmail.com>
2017-09-28 17:28:37 -0400

    core of mongo scraper.  Need to connect to mongodb and then create routes. Not sure what cheerio is...

---
+++ models/Note.js
@@ -0,0 +1,16 @@
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+var NewsSchema= new Schema({
+  title: {
+    type: String
+  },
+  body: {
+    type: String
+  }
+});
+
+// Make a Note model with the NoteSchema
+var News = mongoose.model("News", NewsSchema);
+
+// Export the Note model
+module.exports = News;
